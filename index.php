d5cb1ca30552f4009bcfb5bb912c7222db2e098e
amauryguerin <amauryguerin44@gmail.com>
2023-11-16 16:58:59 +0100

    init v2

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <h1>CSV TO JSON</h1>
 <form name="form" method="post" action="upload.php" enctype="multipart/form-data">
-    <input type="file" name="uploadedFile" required />
+    <input type="file" name="uploadedFile" accept=".csv" required />
     <input type="submit" name="submit" value="upload" />
 </form>
+<a href="version-2.php">VERSION 2 ICI</a>
---
+++ version-2.php
@@ -0,0 +1,6 @@
+<h1>CSV OR XML TO JSON</h1>
+<form name="form" method="post" action="upload-2.php" enctype="multipart/form-data">
+    <input type="file" name="uploadedFile" accept=".csv" required />
+    <input type="submit" name="submit" value="upload" />
+</form>
+<a href="index.php">VERSION 1 ICI</a>
