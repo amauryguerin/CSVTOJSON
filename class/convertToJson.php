230f685cfcaba9ffc4fea745f9f3f4ec5486b900
amauryguerin <amauryguerin44@gmail.com>
2023-11-16 17:55:23 +0100

    v2 update

--- class/convertToJson.php
+++ class/convertToJson.php
@@ -1,10 +1,33 @@
 <?php
 class convertToJson
 {
+    public array $fileUploaded;
+    public string $fileTmpName;
+
     public function convertFileToJson($fileUploaded)
     {
-        // $fileRaw = file_get_contents($fileUploaded, 'r');
-        // $fileName = basename($fileUploaded);
-        var_dump($fileUploaded["type"]);
+        $this->fileUploaded = $fileUploaded;
+        $this->fileTmpName = $this->fileUploaded["tmp_name"];
+
+        $fileRaw = file_get_contents($this->fileTmpName, 'r');
+
+        if ($this->fileUploaded["type"] === "text/csv") {
+            $csvLine = explode("\n", $fileRaw);
+            $csvData = array_map("str_getcsv", $csvLine);
+            $csvJsonEncoded = json_encode($csvData, JSON_PRETTY_PRINT);
+            $fileCleanName = basename($this->fileTmpName) . ".json";
+            if (file_put_contents("uploads/" . $fileCleanName, $csvJsonEncoded)) {
+                echo "<a href='uploads/$fileCleanName' download>Lien de téléchargement du fichier json</a>";
+            }
+        } else {
+            $filechange = str_replace(array("\n", "\r", "\t"), '', $fileRaw);
+            $filetrim = trim(str_replace('"', "'", $filechange));
+            $resultxml = simplexml_load_string($filetrim);
+            $resultjson = json_encode($resultxml);
+            $fileCleanName = basename($this->fileTmpName) . ".json";
+            if (file_put_contents("uploads/" . $fileCleanName, $resultjson)) {
+                echo "<a href='uploads/$fileCleanName' download>Lien de téléchargement du fichier json</a>";
+            }
+        }
     }
 }
--- upload-2.php
+++ upload-2.php
@@ -3,15 +3,3 @@ require_once "./Class/convertToJson.php";
 
 $convertToJson = new convertToJson;
 $convertToJson->convertFileToJson($_FILES['uploadedFile']);
-
-// $filepath = file_get_contents($_FILES['uploadedFile']['tmp_name']);
-// $filechange = str_replace(array("\n", "\r", "\t"), '', $filepath);
-// $filetrim = trim(str_replace('"', "'", $filechange));
-// $resultxml = simplexml_load_string($filetrim);
-// $resultjson = json_encode($resultxml);
-
-// $fileCleanName = basename($_FILES['uploadedFile']['tmp_name']) . ".json";
-
-// if (file_put_contents("uploads/" . $fileCleanName, $resultjson)) {
-//     echo "<a href='uploads/$fileCleanName' download>Lien de téléchargement du fichier json</a>";
-// }
